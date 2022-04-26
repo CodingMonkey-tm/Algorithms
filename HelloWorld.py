5a39a09f413d8da6bf54168229c5d2639d72d583
Coding-Monkey <>
2022-04-26 23:32:54 +0000

    Just enjoy ^_^

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,11 @@
+import random
+
 print('Hello World!!! Welcome to python world!!!')
+print('Good luck with you~~')
+
+lotto = [n for n in range(1, 46)]
+
+print('Go buy lotto now')
+
+for i in range(5):
+  print(random.sample(lotto, 6))
