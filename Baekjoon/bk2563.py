c615222005d08a3d4b66c4c85b63a82aaf1e8138
Coding-Monkey <>
2022-04-26 15:06:23 +0000

    Baekjoon Q2563 Optimize...

--- Baekjoon/bk2563.py
+++ Baekjoon/bk2563.py
@@ -1,33 +1,33 @@
 from sys import stdin
 import time
 
+canvas = [[0 for _ in range(100)] for _ in range(100)]
+
 
 def Input_Data():
-	readl = stdin.readline
-	n = int(readl())
-	papers = [list(map(int, readl().split(' ')))for _ in range(n)]
-	
-	return n, papers
+  readl = stdin.readline
+  n = int(readl())
+  
+  for _ in range(n):
+    x, y = map(int, readl().split(' '))
+    for row in range(y, y+10):
+      for col in range(x, x+10):
+        canvas[row][col] = 1
 
-def Solve(n, papers):
-	# START_TIME = time.time()
+  return n, canvas
 
-	canvas = [[0 for _ in range(100)] for _ in range(100)]
-	res = 0
+def Solve(n, canvas):
+  START_TIME = time.time()
+  
+  res = 0
 
-	for i in range(n):
-		x, y = map(int, papers[i])
-		for h in range(y, y+10):
-			for w in range(x, x+10):
-				canvas[h][w] = 1
+  for row in canvas:
+	  res += sum(row)
 
-	for row in canvas:
-		res += sum(row)
+  END_TIME = time.time()
+  print("time: ", END_TIME - START_TIME)
 
-	# END_TIME = time.time()
-	# print("time: ", END_TIME - START_TIME)
+  return res
 
-	return res
-
-N, papers = Input_Data()
-print(Solve(N, papers))
+N, canvas = Input_Data()
+print(Solve(N, canvas))
